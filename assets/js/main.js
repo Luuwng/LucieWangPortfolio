4bd41f8922114a7b949714b81a3a0ac38fa83790
Luuwng <150408637+Luuwng@users.noreply.github.com>
2024-02-28 12:32:24 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function openNav() {
+   	document.getElementById('navigation').style.display = 'block';
+  }
+function closeNav() {
+   	document.getElementById('navigation').style.display = 'none';
+  }
